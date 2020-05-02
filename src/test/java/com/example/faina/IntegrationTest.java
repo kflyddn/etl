ea8c71147a4479d8d90b228d68efd17e29ecf9fe
sunny14 <faina.private@gmail.com>
2020-05-02 14:04:26 +0300

    test configuration added integration test can send csv (still cannot receive csv/json)

--- src/test/java/com/example/faina/IntegrationTest.java
+++ src/test/java/com/example/faina/IntegrationTest.java
@@ -1,30 +1,23 @@
 package com.example.faina;
 
-import com.example.faina.config.KafkaTopicConfig;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
+import com.example.faina.config.TestConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.junit.After;
-import org.junit.ClassRule;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.core.*;
-import org.springframework.kafka.listener.ContainerProperties;
 import org.springframework.kafka.listener.KafkaMessageListenerContainer;
-import org.springframework.kafka.listener.MessageListener;
-import org.springframework.kafka.test.rule.EmbeddedKafkaRule;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
 import org.springframework.kafka.test.utils.ContainerTestUtils;
-import org.springframework.kafka.test.utils.KafkaTestUtils;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.Map;
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 
 import static com.example.faina.config.KafkaTopicConfig.*;
@@ -32,45 +25,28 @@ import static com.example.faina.utils.MessageUtils.sendMessage;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = {KafkaTopicConfig.class})
+@SpringBootTest(classes = {TestConfig.class})
 @DirtiesContext
 public class IntegrationTest {
 
     private static Logger logger = LoggerFactory.getLogger(IntegrationTest.class);
 
-    @ClassRule
-    public static EmbeddedKafkaRule embeddedKafka = new EmbeddedKafkaRule(1, true);
+    @Autowired
+    private EmbeddedKafkaBroker kafkaBroker;
 
-    @Value(value = "${kafka.bootstrapAddress}")
-    private String bootstrapAddress;
-
-    @Value(value = "${kafka.groupId}")
-    private String groupId;
-
-
+    @Autowired
     private KafkaMessageListenerContainer<String, String> container;
 
+    @Autowired
+    private  KafkaTemplate<String, String> kafkaTemplate;
+
+    @Autowired
     private BlockingQueue<ConsumerRecord<String, String>> consumerRecords;
 
     @BeforeEach
     public void setUp() {
-        consumerRecords = new LinkedBlockingQueue<>();
-
-        ContainerProperties containerProperties = new ContainerProperties(JSON_TOPIC);
-
-        Map<String, Object> consumerProperties = KafkaTestUtils.consumerProps(
-                groupId, "true", embeddedKafka.getEmbeddedKafka());
-
-        DefaultKafkaConsumerFactory<String, String> consumer = new DefaultKafkaConsumerFactory<>(consumerProperties);
-
-        container = new KafkaMessageListenerContainer<>(consumer, containerProperties);
-        container.setupMessageListener((MessageListener<String, String>) record -> {
-            logger.debug("Listened message='{}'", record.toString());
-            consumerRecords.add(record);
-        });
         container.start();
-
-        ContainerTestUtils.waitForAssignment(container, embeddedKafka.getEmbeddedKafka().getPartitionsPerTopic());
+        ContainerTestUtils.waitForAssignment(container, kafkaBroker.getPartitionsPerTopic());
     }
 
     @After
@@ -82,28 +58,16 @@ public class IntegrationTest {
     @Test
     public void csvFlowTest() throws Exception {
 
-     //   container.setBeanName("templateTests");
-        Map<String, Object> producerProps =
-                KafkaTestUtils.producerProps(embeddedKafka.getEmbeddedKafka());
-        producerProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                bootstrapAddress);
-        ProducerFactory<String, String> pf =
-                new DefaultKafkaProducerFactory<String, String>(producerProps);
-       KafkaTemplate<String, String> template = new KafkaTemplate<>(pf);
-        //TODO: assert that records is empty before sending
-       // assertThat(records.poll(10, TimeUnit.SECONDS), hasValue("foo"));
-        //TODO: fix data
-        sendMessage(CSV_TOPIC, "bar", template, logger);
+        assertTrue(consumerRecords.isEmpty());
+        String csvMessage = "id,header1, header2\n" +
+                "1,val1,val2\n" +
+                "2,val11,val22\n" +
+                "3,val111,val222";
+        sendMessage(CSV_TOPIC, csvMessage, kafkaTemplate, logger);
         ConsumerRecord<String, String> received = consumerRecords.poll(10, TimeUnit.SECONDS);
         assertTrue(received != null);
-       /* assertThat(received, hasKey(2));
-        assertThat(received, hasPartition(0));
-        assertThat(received, hasValue("bar"));
-        template.send(TEMPLATE_TOPIC, 0, 2, "baz");
-        received = records.poll(10, TimeUnit.SECONDS);
-        assertThat(received, hasKey(2));
-        assertThat(received, hasPartition(0));
-        assertThat(received, hasValue("baz"));*/
+        String expected = "";
+        assertTrue(received.value().equals(expected));
     }
 
        /* @Test
---
+++ src/test/java/com/example/faina/config/TestConfig.java
@@ -0,0 +1,90 @@
+package com.example.faina.config;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.junit.ClassRule;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.listener.KafkaMessageListenerContainer;
+import org.springframework.kafka.listener.MessageListener;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
+import org.springframework.kafka.test.rule.EmbeddedKafkaRule;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
+
+import java.util.Map;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import static com.example.faina.config.KafkaTopicConfig.*;
+
+@Configuration
+public class TestConfig {
+
+    private static final String [] topics  = {XML_TOPIC, CSV_TOPIC, ERROR_TOPIC, JSON_TOPIC};
+
+    @Value(value = "${kafka.bootstrapAddress}")
+    private String bootstrapAddress;
+
+
+    @Value(value = "${kafka.groupId}")
+    private String groupId;
+
+    @ClassRule
+    public static EmbeddedKafkaRule embeddedKafka = new EmbeddedKafkaRule(1, true, topics);
+
+    @Bean
+    public EmbeddedKafkaBroker kafkaBroker() {
+        //set port
+        embeddedKafka.kafkaPorts(9092);
+
+        return embeddedKafka.getEmbeddedKafka();
+
+    }
+
+    @Bean
+    public BlockingQueue<ConsumerRecord<String, String>> consumerRecords()  {
+        return new LinkedBlockingQueue<>();
+    }
+
+
+
+    @Bean
+    public KafkaMessageListenerContainer<String, String> embeddedContainer(@Autowired EmbeddedKafkaBroker kafkaBroker,
+                                                                           @Autowired BlockingQueue<ConsumerRecord<String, String>> consumerRecords)    {
+        ContainerProperties containerProperties = new ContainerProperties(JSON_TOPIC);
+
+        Map<String, Object> consumerProperties = KafkaTestUtils.consumerProps(
+                groupId, "true", kafkaBroker);
+        consumerProperties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                bootstrapAddress);
+
+        DefaultKafkaConsumerFactory<String, String> consumer = new DefaultKafkaConsumerFactory<>(consumerProperties);
+
+        KafkaMessageListenerContainer<String, String> container =  new KafkaMessageListenerContainer<>(consumer, containerProperties);
+
+        container.setupMessageListener((MessageListener<String, String>) record -> {
+            System.out.println("test listener got message:\n"+record.toString());
+            consumerRecords.add(record);
+        });
+
+        return container;
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate (@Autowired EmbeddedKafkaBroker kafkaBroker)    {
+        Map<String, Object> producerProps =
+                KafkaTestUtils.producerProps(kafkaBroker);
+        producerProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                bootstrapAddress);
+        ProducerFactory<String, String> pf =
+                new DefaultKafkaProducerFactory<String, String>(producerProps);
+        return new KafkaTemplate<>(pf);
+    }
+}
